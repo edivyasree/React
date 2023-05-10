bddcf4e235a9d8c7b2ce8e4da24f405d19881978
edivyasree <edivyasree@gmail.com>
2023-05-10 11:44:13 +0530

    newcommit

--- src/components/Callus.js
+++ src/components/Callus.js
@@ -1,9 +1,16 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-function Callus(){
+import React, { useState } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+function Callus(props){
 const navigate=useNavigate()
+console.log(navigate);
+const location=useLocation()
+console.log(location);
+     const [name,setname]=useState("divya")
     return(
         <div>
+          <h1>Users page</h1>
+      <p>{location.pathname}</p>
+      <p>{new URLSearchParams(location.search).get('name')}</p>   
      <h1>8774212120</h1>
         </div>
     )
